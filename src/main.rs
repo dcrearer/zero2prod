beb38c964e1a50f0efe04caa4cda6c14ad388b50
dcrearer <dcrearer@gmail.com>
2024-12-17 11:46:28 -0500

    add connection pooling & configuration artifacts

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 //! src/main.rs
 
 use std::net::TcpListener;
-use zero2prod::run;
+use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
+use sqlx::{PgPool};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("0.0.0.0:8000").expect("Failed to bind random port");
+    let configuration = get_configuration()
+        .expect("Failed to read configuration");
 
-    run(listener)?.await
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+
+    run(listener, connection_pool)?.await
 }
