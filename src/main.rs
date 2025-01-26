531dbfab7d54139e5b682382db35cba8559fdec0
dcrearer <dcrearer@gmail.com>
2025-01-25 22:30:20 -0500

    sign up a new subscriber

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+//! src/main.rs
+//!
+use std::net::TcpListener;
+use sqlx::PgPool;
+use zero2prod::startup;
+use zero2prod::configuration::get_configuration;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let configuration = get_configuration()
+        .expect("Failed to read configuration.");
+
+    let connection_pool = PgPool::connect(
+        &configuration.database.connection_string()
+    ).await.expect("Failed to connect to Postgres.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+
+    startup::run(listener, connection_pool)?.await
 }
