33be6dbca584c3caa18e1d0bd88b727df52d10c1
dcrearer <dcrearer@gmail.com>
2025-05-12 10:54:27 -0400

    login hmac implementation

--- src/startup.rs
+++ src/startup.rs
@@ -1,9 +1,12 @@
 //! src/startup.rs
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::{confirm, health_check, publish_newsletters, subscribe};
+use crate::routes::{
+    confirm, health_check, home, login, login_form, publish_newsletters, subscribe,
+};
 use actix_web::dev::Server;
 use actix_web::{App, HttpServer, web};
+use secrecy::Secret;
 use sqlx::PgPool;
 use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
@@ -39,6 +42,7 @@ impl Application {
             connection_pool,
             email_client,
             configuration.application.base_url,
+            configuration.application.hmac_secret,
         )?;
 
         Ok(Self { port, server })
@@ -64,6 +68,7 @@ pub fn run(
     db_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
+    hmac_secret: Secret<String>,
 ) -> Result<Server, std::io::Error> {
     let db_pool = web::Data::new(db_pool);
     let email_client = web::Data::new(email_client);
@@ -75,12 +80,19 @@ pub fn run(
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
             .route("/newsletters", web::post().to(publish_newsletters))
+            .route("/", web::get().to(home))
+            .route("/login", web::get().to(login_form))
+            .route("/login", web::post().to(login))
             .app_data(base_url.clone())
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
+            .app_data(web::Data::new(HmacSecret(hmac_secret.clone())))
     })
     .listen(listener)?
     .run();
 
     Ok(server)
 }
+
+#[derive(Clone, Debug)]
+pub struct HmacSecret(pub Secret<String>);
