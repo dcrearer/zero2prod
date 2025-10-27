008ec73e887c2ad1fdad5fb15f6249fcbf56a03a
Dwayne Crearer <dcrearer@gmail.com>
2025-10-27 14:34:35 -0400

    Implemented Foundational Telemetry

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,24 @@
 //! tests/health_check.rs
+use secrecy::{ExposeSecret, Secret};
+use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
-use zero2prod::configuration::{get_configuration, DatabaseSettings};
-use sqlx::{PgPool, Connection, Executor, PgConnection};
-use zero2prod::startup::run;
+use std::sync::LazyLock;
 use uuid::Uuid;
+use zero2prod::configuration::{DatabaseSettings, get_configuration};
+use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
+static TRACING: LazyLock<()> = LazyLock::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    }
+});
 
 pub struct TestApp {
     pub address: String,
@@ -12,9 +26,9 @@ pub struct TestApp {
 }
 
 async fn spawn_app() -> TestApp {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind random port");
+    LazyLock::force(&TRACING);
 
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
     let mut configuration = get_configuration().expect("Failed to read configuration");
@@ -22,35 +36,35 @@ async fn spawn_app() -> TestApp {
 
     let connection_pool = configure_database(&configuration.database).await;
 
-    let server = run(listener, connection_pool.clone())
-        .expect("Failed to bind address");
+    let server = run(listener, connection_pool.clone()).expect("Failed to bind address");
 
     let _ = tokio::spawn(server);
 
-    TestApp { address, db_pool: connection_pool }
+    TestApp {
+        address,
+        db_pool: connection_pool,
+    }
 }
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
     let maintenance_settings = DatabaseSettings {
         database_name: "postgres".to_string(),
         username: "postgres".to_string(),
-        password: "password".to_string(),
+        password: Secret::new("password".to_string()),
         ..config.clone()
     };
 
-    let mut connection = PgConnection::connect(
-        &maintenance_settings.connection_string(),
-    )
-    .await
-    .expect("Failed to connect to Postgres");
+    let mut connection =
+        PgConnection::connect(&maintenance_settings.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres");
 
-    connection.execute(
-        format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str()
-    )
-    .await
-    .expect("Failed to create database");
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .await
+        .expect("Failed to create database");
 
-    let connection_pool = PgPool::connect(&config.connection_string())
+    let connection_pool = PgPool::connect(&config.connection_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres");
 
@@ -66,7 +80,6 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
 async fn health_check() {
     let app = spawn_app().await;
     let client = reqwest::Client::new();
-
     let response = client
         .get(format!("{}/health_check", &app.address))
         .send()
@@ -80,7 +93,6 @@ async fn health_check() {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app = spawn_app().await;
-
     let client = reqwest::Client::new();
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let response = client
