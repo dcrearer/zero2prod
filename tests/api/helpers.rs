461fa0807bb43978f4addba0bc0507bfaea5ac59
Dwayne Crearer <dcrearer@gmail.com>
2025-12-27 06:54:52 -0500

    reject invalid subscribers #1

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,8 +1,11 @@
 //! tests/api/helpers.rs
+use assert2::assert;
+use reqwest::Url;
 use secrecy::SecretString;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::sync::LazyLock;
 use uuid::Uuid;
+use wiremock::MockServer;
 use zero2prod::configuration::{DatabaseSettings, get_configuration};
 use zero2prod::startup::{Application, get_connection_pool};
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
@@ -22,16 +25,25 @@ static TRACING: LazyLock<()> = LazyLock::new(|| {
 
 pub struct TestApp {
     pub address: String,
+    pub port: u16,
     pub db_pool: PgPool,
+    pub email_server: MockServer,
+}
+
+pub struct ConfirmationLinks {
+    pub html: reqwest::Url,
+    pub plain_text: reqwest::Url,
 }
 
 pub async fn spawn_app() -> TestApp {
     LazyLock::force(&TRACING);
 
+    let email_server = MockServer::start().await;
     let configuration = {
         let mut c = get_configuration().expect("Failed to read configuration.");
         c.database.database_name = Uuid::new_v4().to_string();
         c.application.port = 0;
+        c.email_client.base_url = email_server.uri();
         c
     };
     configure_database(&configuration.database).await;
@@ -40,13 +52,16 @@ pub async fn spawn_app() -> TestApp {
         .await
         .expect("Failed to build application");
 
-    let address = format!("Http://127.0.0.1:{}", application.port());
+    // let address = format!("Http://127.0.0.1:{}", application.port());
+    let application_port = application.port();
     // Intentionally discarding the JoinHandle without triggering clippy warnings.
     drop(tokio::spawn(application.run_until_stopped()));
 
     TestApp {
-        address,
+        address: format!("http://localhost:{}", application_port),
         db_pool: get_connection_pool(&configuration.database),
+        email_server,
+        port: application_port,
     }
 }
 
@@ -84,11 +99,36 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
 impl TestApp {
     pub async fn post_subscriptions(&self, body: String) -> reqwest::Response {
         reqwest::Client::new()
-            .post(&format!("{}/subscriptions", &self.address))
+            .post(format!("{}/subscriptions", &self.address))
             .header("Content-Type", "application/x-www-form-urlencoded")
             .body(body)
             .send()
             .await
             .expect("Failed to execute request.")
+    }
+
+    pub fn get_confirmation_links(&self, email_request: &wiremock::Request) -> ConfirmationLinks {
+        let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
+
+        let get_link = |s: &str| {
+            let links: Vec<_> = linkify::LinkFinder::new()
+                .links(s)
+                .filter(|l| *l.kind() == linkify::LinkKind::Url)
+                .collect();
+
+            assert!(links.len() == 1);
+
+            let raw_link = links[0].as_str().to_owned();
+            let mut confirmation_link = Url::parse(&raw_link).unwrap();
+
+            assert!(confirmation_link.host_str().unwrap() == "127.0.0.1");
+
+            confirmation_link.set_port(Some(self.port)).unwrap();
+            confirmation_link
+        };
+
+        let html = get_link(body["HtmlBody"].as_str().unwrap());
+        let plain_text = get_link(body["TextBody"].as_str().unwrap());
+        ConfirmationLinks { html, plain_text }
     }
 }
